3004d7073bd4ff1de194cbcdd2e09049993bb073
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-06-18 20:00:45 +0530

    Delete shell-scripts directory

--- shell-scripts/script1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-current_date_time=$(date)
-username=$(whoami)
-current_directory=$(pwd)
-
-echo "Current date & time : $current_date_time"
-echo "Username : $username"
-echo "Current working directory : $current_directory"
