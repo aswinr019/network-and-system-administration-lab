91e94ca18bd462e3354a7d5ac81629c68e44c89d
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-06-19 22:33:05 +0530

    Update script-11.sh

--- shell-scripts/script-11.sh
+++ shell-scripts/script-11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Write a shell script to validate password strenght . Here are a few assumptions for the password string.
-# Length -  minimum character lenght is eight. Contain both alphabet and number. Include both small and capital case letters.
+# Write a shell script to validate password strength . Here are a few assumptions for the password string.
+# Length -  minimum character length is eight. Contain both alphabet and number. Include both small and capital case letters.
 # If the password doesn't comply with any of the above conditions , then the scipt should report it as "weak password".
 
 read -p "enter password: " password
