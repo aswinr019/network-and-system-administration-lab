12cc2d2bbd69a09047d7fe49be50f142a5d51496
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-08-08 20:47:49 +0530

    Update script-11.sh

--- shell-scripts/script-11.sh
+++ shell-scripts/script-11.sh
@@ -4,23 +4,20 @@
 # Length -  minimum character length is eight. Contain both alphabet and number. Include both small and capital case letters.
 # If the password doesn't comply with any of the above conditions , then the scipt should report it as "weak password".
 
-read -p "enter password: " password
+
+read -p "Enter a password to check : " password
 
 
-length=${#password}
-
-if [ -n "$password" ]; then 
-	if [ $length -gt 7 ]; then
-		if  [[ "$password" =~ ^[[:alnum:]]+$ ]];  then
-			if [[ "$password" =~ [[:lower:]] && "$password" =~ [[:upper:]] ]]; then
-				echo "strong password"
-			else
-				echo "weak password!"
-			fi
-		fi
-	else
-		echo "weak password!"
-	fi
-else 
-	echo "please enter a password!"
+if [[  "${#password}" -lt 10 ]]; then
+	echo "Weak password : Password should atleast contain 10 characters!"
+elif [[ ! "$password" =~ [a-z] ]]; then
+	echo "Weak password : Password should atleast contain one lowercase character"
+elif [[ ! "$password" =~ [A-Z] ]]; then 
+	echo "Weak password : password should atleast contain one uppercase character"
+elif [[ ! "$password" =~ [0-9] ]]; then
+	echo "Weak password : password should atlest contain one number"
+elif [[ ! "$password" =~ [!@#$%*()] ]]; then
+	echo "Weak password : password should atleast contain one special charcter"
+else
+	echo "Strong password!"
 fi
